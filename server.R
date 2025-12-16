f8481dc1e1489fd4f3b82395814ad7bc5612fa9c
Extremys <valentinplanes@live.fr>
2025-12-16 15:42:55 +0100

    Update server.R

--- server.R
+++ server.R
@@ -1,10 +1,9 @@
-server.R
 library(shiny)
 library(RCurl) # 1. Load the RCurl package
 
 # --- RCurl Feasibility Test ---
 # Define a well-known public URL to test connectivity
-public_url <- "https://www.r-project.org/" 
+public_url <- "https://www.r-project.org/"
 
 # Use url.exists() to check if the URL is accessible
 # This is executed once when the server logic is initialized
@@ -21,20 +20,20 @@ cat("*****************************\n\n")
 
 # Define server logic required to generate and plot a random distribution
 shinyServer(function(input, output) {
-  
+
   # Expression that generates a plot of the distribution. The expression
   # is wrapped in a call to renderPlot to indicate that:
   #
-  #  1) It is "reactive" and therefore should be automatically 
+  #  1) It is "reactive" and therefore should be automatically
   #     re-executed when inputs change
-  #  2) Its output type is a plot 
+  #  2) Its output type is a plot
   #
   output$distPlot <- renderPlot({
-    
+
     # generate an rnorm distribution and plot it
     # Note: 'input$obs' needs to be defined in your 'ui.R' (e.g., a slider)
     dist <- rnorm(input$obs)
     hist(dist)
   })
-  
+
 })
